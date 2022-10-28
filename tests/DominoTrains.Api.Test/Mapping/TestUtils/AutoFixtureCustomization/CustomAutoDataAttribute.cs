8e5d3343ce06cab85559238c39e5673c4a4fa0f6
Ben Wong <42486090+bwong365@users.noreply.github.com>
2022-10-28 08:56:16 -0600

    Use static method for AutoData fixture creation

--- tests/DominoTrains.Api.Test/Mapping/TestUtils/AutoFixtureCustomization/CustomAutoDataAttribute.cs
+++ tests/DominoTrains.Api.Test/Mapping/TestUtils/AutoFixtureCustomization/CustomAutoDataAttribute.cs
@@ -5,14 +5,16 @@ namespace DominoTrains.Api.Test.Mapping.TestUtils.AutoFixtureCustomization;
 
 public sealed class CustomAutoDataAttribute : AutoDataAttribute
 {
-    public CustomAutoDataAttribute() : base(() =>
+    public CustomAutoDataAttribute() : base(CreateFixture)
+    {
+    }
+
+    private static Fixture CreateFixture()
     {
         var fixture = new Fixture();
 
         fixture.Customize(new DominoTrainCustomization());
 
         return fixture;
-    })
-    {
     }
 }
